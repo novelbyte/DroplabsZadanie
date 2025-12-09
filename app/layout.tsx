2a13562a0dcf993dbbc3ca8d5c0e1f444d00440d
kacper.pieron <kpieron@poczta.fm>
2025-12-09 10:01:19 +0100

    pierwszy commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >d 
+      >d da
         {children}
       </body>
     </html>
