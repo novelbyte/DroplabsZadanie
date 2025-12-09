9c2725e8cddcb326c8fcab9b4cb289f83e56f630
kacper.pieron <kpieron@poczta.fm>
2025-12-09 10:05:59 +0100

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >d dadagit push --set-upstream origin master
-dada
+dadada
         {children}
       </body>
     </html>
